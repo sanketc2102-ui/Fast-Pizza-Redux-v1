3db24b0b92f940df3659ffa6fa5d2bd2ac7f1413
Sanket Chavan <sanketc2102@gmail.com>
2026-07-02 23:00:07 +0530

    impliment loader but react router style

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const data = useNavigation();
+
+  const isLoading = data.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
 
       <main>
+        {" "}
         <Outlet />
       </main>
 
